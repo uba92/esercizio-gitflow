ee9ee66becc05f81f8079ad0c41bd0ba455a6c71
Umberto Tramontano <u.tramontano92@libero.it>
2025-02-20 17:14:24 +0100

    Initial commit

---
+++ src/main/java/it/epicode/esercizio_gitflow/EsercizioGitflowApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.esercizio_gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioGitflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioGitflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/esercizio_gitflow/EsercizioGitflowApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.esercizio_gitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioGitflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
